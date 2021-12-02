b53eaca9d7ed0be8c293857ea063e0a4aa42a545
Philipp <pd95@users.noreply.github.com>
2021-12-02 21:05:06 +0100

    Basic LockScreen - background image - Time / Date - Flashlight and camera button (with haptic feedback on activation) - Home indicator

--- LockScreen/ContentView.swift
+++ LockScreen/ContentView.swift
@@ -7,10 +7,76 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct LockScreenButton: View {
+    @State private var pressed = false
+    @State private var activated = false
+
+    let image: String
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Image(systemName: image)
+            .font(.title3)
+            .frame(width: 50, height: 50)
+            .background(activated ? Color.white :
+                            Color.black.opacity(pressed ? 0.8 : 0.4))
+            .foregroundColor(activated ? .black : .white)
+            .clipShape(Circle())
+            .scaleEffect(pressed ? 1.5 : 1)
+            .animation(.spring(response: 0.5, dampingFraction: 0.7), value: pressed)
+            .onLongPressGesture(minimumDuration: 0.4) { bool in
+                pressed = bool
+            } perform: {
+                activated.toggle()
+                pressed = false
+
+                let generator = UIImpactFeedbackGenerator()
+                generator.impactOccurred()
+            }
+    }
+}
+
+struct ContentView: View{
+
+    var body: some View {
+        ZStack {
+            GeometryReader { geo in
+                Image(decorative: "bigsur")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(maxWidth: geo.size.width)
+
+                Color.black.opacity(0.15)
+
+                VStack(spacing: 0) {
+                    Image(systemName: "lock.fill")
+                        .font(.largeTitle)
+                        .padding(.top, 60)
+
+                    Text(Date(), style: .time)
+                        .font(.system(size: 82, weight: .thin))
+
+                    Text(Date(), style: .date)
+                        .font(.title2)
+                        .offset(y: -10)
+
+                    Spacer()
+
+                    HStack {
+                        LockScreenButton(image: "flashlight.off.fill")
+                        Spacer()
+                        LockScreenButton(image: "camera.fill")
+                    }
+                    .padding([.leading, .trailing, .bottom])
+
+                    Capsule()
+                        .fill(Color.white)
+                        .frame(width: 150, height: 5)
+                        .padding(.bottom, 10)
+                }
+                .padding([.leading, .trailing])
+                .foregroundColor(.white)
+            }
+        }.ignoresSafeArea()
     }
 }
 
