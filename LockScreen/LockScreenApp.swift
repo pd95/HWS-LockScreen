454efa9a7bce3c08797956a5c3ac2c4197f17358
Philipp <pd95@users.noreply.github.com>
2021-12-02 20:24:39 +0100

    Initial Commit

---
+++ LockScreen/LockScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockScreenApp.swift
+//  LockScreen
+//
+//  Created by Philipp on 02.12.21.
+//
+
+import SwiftUI
+
+@main
+struct LockScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
